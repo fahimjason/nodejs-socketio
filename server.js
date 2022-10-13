51fe846d7f0ac012b8c96156cc1c68568a74edb0
Fahimul Hoque <fahimulhoquejason@gmail.com>
2022-10-13 21:28:43 +0600

    Added mongodb

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+const dotenv = require('dotenv');
 const express = require("express");
+const mongoose = require('mongoose');
+
+dotenv.config();
 
 const app = express();
 
@@ -9,25 +13,37 @@ app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const messages = [
-  { name: "John", message: "Hello" },
-  { name: "Doe", message: "Hi" },
-];
+const Message = mongoose.model('message', {
+    name: String,
+    message: String
+});
 
 app.get("/messages", (req, res) => {
-  res.send(messages);
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    })
 });
 
 app.post("/messages", (req, res) => {
-    messages.push(req.body);
-    io.emit(req.body);
-    res.sendStatus(200);
+    const message = new Message(req.body);
+
+    message.save((err) => {
+        if (err)
+            sendStatus(500);
+
+        io.emit(req.body);
+        res.sendStatus(200);
+    })
 });
 
 io.on("connection", () => {
     console.log('Connected')
-})
+});
+
+mongoose.connect(process.env.MONGO_URI, (err) => {
+    console.log('DB connected');
+});
 
 const server = http.listen(5000, () => {
-  console.log("Server is listening on port", server.address().port);
+    console.log("Server is listening on port", server.address().port);
 });
